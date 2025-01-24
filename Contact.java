da9e47ed0ad1f5dfb96ea59383c6741d2762d701
keerthigrandhi <147238003+keerthigrandhi@users.noreply.github.com>
2025-01-24 16:11:05 +0530

    Create Contact.java

---
+++ Contact.java
@@ -0,0 +1,36 @@
+
+public class Contact {
+	private String name;
+	private long mobileNumber;
+	private String type;
+	public Contact(String name, long mobileNumber, String type) {
+		this.name = name;
+		this.mobileNumber = mobileNumber;
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	@Override
+	public String toString() {
+		return "Contact [name=" + name + ", mobileNumber=" + mobileNumber + ", type=" + type + "]";
+	}
+	
+	
+	
+}
